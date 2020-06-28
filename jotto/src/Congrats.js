41745c572740a39ecb3af4d1e94a070bfa420c91
gonzs <gonzalosisnero@gmail.com>
2020-06-28 20:34:56 -0300

    congrats component with render test

--- jotto/src/Congrats.js
+++ jotto/src/Congrats.js
@@ -3,8 +3,17 @@ import React from "react";
 /**
  * Functional react component for congratulory message
  * @function
- * @return {JSX.Element}
+ * @param {object} props - React props
+ * @return {JSX.Element} -Rendered component (or  null if succes prop is false)
  */
-export default () => {
-  return <div />;
+export default ({ success }) => {
+  if (success)
+    return (
+      <div data-test="component-congrats">
+        <span data-test="congrats-message">
+          Congratulations! You guessed the word
+        </span>
+      </div>
+    );
+  else return <div data-test="component-congrats" />;
 };
